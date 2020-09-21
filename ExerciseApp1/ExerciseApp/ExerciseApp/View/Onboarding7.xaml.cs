8592feb37d766277055e80abe6633d122574ab8b
Scorchedwarf <danielbarnett@live.com>
2020-09-21 14:45:21 +0100

    Changed toolbar to dark

---
+++ ExerciseApp1/ExerciseApp/ExerciseApp/View/Onboarding7.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExerciseApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Onboarding7 : ContentView
+    {
+        public Onboarding7()
+        {
+            InitializeComponent();
+        }
+    }
+}
