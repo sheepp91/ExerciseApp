741cb0cde998a320c13635335605c0074ffced05
Scorchedwarf <danielbarnett@live.com>
2020-10-17 15:24:22 +0100

    Home page

---
+++ ExerciseApp1/ExerciseApp/ExerciseApp/View/Home.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExerciseApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : ContentPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+            stkTab1.IsVisible = true;
+            stkTab2.IsVisible = false;
+
+        }
+
+        private void Tab1Clicked(object sender, EventArgs e)
+        {
+            stkTab1.IsVisible = true;
+            stkTab2.IsVisible = false;
+        }
+
+        private void Tab2Clicked(object sender, EventArgs e)
+        {
+            stkTab1.IsVisible = false;
+            stkTab2.IsVisible = true;
+        }
+
+    }
+}
